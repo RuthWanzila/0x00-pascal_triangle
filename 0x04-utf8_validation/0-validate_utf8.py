2588dd23a6779677adc5dec7343b0362675223ff
RuthWanzila <wanzilaruth533@gmail.com>
2024-05-23 10:01:43 +0000

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,7 @@ def validUTF8(data):
     Method that determines if a given data set represents a valid
     UTF-8 encoding.
     """
-    bytes_no. = 0
+    bytes_number = 0
 
     bitsmask1 = 1 << 7
     bitsmask2 = 1 << 6
@@ -16,25 +16,25 @@ def validUTF8(data):
 
         bitsmask_byte = 1 << 7
 
-        if bytes_no. == 0:
+        if bytes_number == 0:
 
             while bitsmask_byte & i:
-                bytes_no. += 1
+                bytes_number += 1
                 bitsmask_byte = bitsmask_byte >> 1
 
-            if bytes_no. == 0:
+            if bytes_number == 0:
                 continue
 
-            if bytes_no. == 1 or bytes_no. > 4:
+            if bytes_number == 1 or bytes_number > 4:
                 return False
 
         else:
             if not (i & bitsmask1 and not (i & bitsmask2)):
                     return False
 
-        bytes_no. -= 1
+        bytes_number -= 1
 
-    if bytes_no. == 0:
+    if bytes_number == 0:
         return True
 
     return False
